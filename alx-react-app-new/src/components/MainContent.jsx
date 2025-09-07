c7f51c647dca033a71d56def273522a579c579f7
fatyyyjb <jbariiifatima@gmail.com>
2025-09-07 23:36:15 +0100

    push

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', textAlign: 'center', backgroundColor: '#eef' }}>
+      <h2>Welcome to my profile</h2>
+      <p>This is the main content section with my favorite cities listed below.</p>
+    </main>
+  );
+}
+
+export default MainContent;
