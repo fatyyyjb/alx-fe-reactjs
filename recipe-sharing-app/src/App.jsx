13cafdadccd66c5d2e786cdd4056d94207f5599a
fatyyyjb <jbariiifatima@gmail.com>
2025-09-14 21:30:42 +0100

    push

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>🍳 Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  )
+}
+
+export default App
