e9df4acf479d926ef34186e5d8c9cfb0e37dcba3
fatyyyjb <jbariiifatima@gmail.com>
2025-09-24 15:37:22 +0100

    push.

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,25 +1,22 @@
 import axios from "axios";
 
-export async function fetchAdvancedUsers(username, location, minRepos) {
+// Simple user fetch by username (Task 1)
+export async function fetchUserData(username) {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+}
+
+// Advanced user search (Task 2)
+export async function advancedUserSearch(username, location, minRepos) {
   let query = "";
 
-  if (username) query += `${username}+`;
-  if (location) query += `location:${location}+`;
+  if (username) query += `${username} in:login `;
+  if (location) query += `location:${location} `;
   if (minRepos) query += `repos:>=${minRepos}`;
 
   const response = await axios.get(
-    `https://api.github.com/search/users?q=${query}&per_page=10`
+    `https://api.github.com/search/users?q=${query.trim()}`
   );
 
-  const users = response.data.items;
-
-  // Fetch more details for each user (location, repo count, etc.)
-  const detailedUsers = await Promise.all(
-    users.map(async (user) => {
-      const details = await axios.get(user.url);
-      return { ...user, ...details.data };
-    })
-  );
-
-  return detailedUsers;
+  return response.data; // { total_count, items }
 }
